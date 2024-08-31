96eacb669aafae1c7d83b3db73ec2bce55677ef0
Ewenn <fleter.ewenn@gmail.com>
2024-08-31 15:27:50 +0200

    creation de la fenetre

--- space_simulation.py
+++ space_simulation.py
@@ -1,3 +1,13 @@
 import pygame
 
 pygame.init()
+
+screen = pygame.display.set_mode((850, 850))
+pygame.display.set_caption('space_simulation')
+
+run = True
+
+While run:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT():
+      run = False
