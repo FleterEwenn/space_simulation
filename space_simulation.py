c20c1b452a99a0dbe91cfb97830d3ad7e246ad82
FleterEwenn <fleter.ewenn@gmail.com>
2024-08-31 15:33:18 +0200

    ajustements

--- space_simulation.py
+++ space_simulation.py
@@ -2,12 +2,12 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((850, 850))
+screen = pygame.display.set_mode((1100, 900))
 pygame.display.set_caption('space_simulation')
 
 run = True
 
-While run:
+while run:
   for event in pygame.event.get():
-    if event.type == pygame.QUIT():
+    if event.type == pygame.QUIT:
       run = False
