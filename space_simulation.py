8322f9fc3b9bbcabdcd2e09a4c40001d90d68355
Ewenn <fleter.ewenn@gmail.com>
2024-09-05 09:41:02 +0200

    ajout des cercles

--- space_simulation.py
+++ space_simulation.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -7,9 +8,18 @@ pygame.display.set_caption('space_simulation')
 
 run = True
 
+rayon_1 = random.randint(10, 30)
+rayon_2 = random.randint(35, 60)
+
+pos_1 = (random.randint(100, 500), random.randint(50, 850))
+pos_2 = (random.randint(600, 1000), random.randint(50, 850))
+
 while run:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       run = False
+
+  pygame.draw.circle(screen, (150, 150, 150), pos_1, rayon_1)
+  pygame.draw.circle(screen, (150, 150, 150), pos_2, rayon_2)
 
 pygame.quit()
