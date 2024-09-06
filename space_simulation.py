8a25998189662208b855c3d0fa74b3a19504f483
Ewenn <fleter.ewenn@gmail.com>
2024-09-06 18:11:54 +0200

    ajout de la classe Planet

--- space_simulation.py
+++ space_simulation.py
@@ -6,24 +6,33 @@ pygame.init()
 screen = pygame.display.set_mode((1100, 900))
 pygame.display.set_caption('space_simulation')
 
+class Planet(pygame.sprite.Sprite):
+  def __init__(self):
+    super().__init__()
+    self.all_planets = pygame.sprite.Group()
+
+    pygame.draw.circle(screen, (150, 150, 150), (0, 0), random.randint(10, 60))
+    
+planet = Planet()
+planet.x = random.randint(100, 1000)
+planet.y = random.randint(50, 850)
+  
+Planet().all_planets.add(planet) 
+
 run = True
-
-rayon_1 = random.randint(10, 30)
-rayon_2 = random.randint(35, 60)
-
-pos_1 = (random.randint(100, 500), random.randint(50, 850))
-pos_2 = (random.randint(600, 1000), random.randint(50, 850))
 
 while run:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       run = False
-
+      
+  Planet().all_planets.update()
+  
   screen.fill((0, 0, 0))
 
-  pygame.draw.circle(screen, (150, 150, 150), pos_1, rayon_1)
-  pygame.draw.circle(screen, (150, 150, 150), pos_2, rayon_2)
+  Planet().all_planets.draw(screen)
+  
+  #pygame.draw.circle(screen, (150, 150, 150), pos_1, rayon_1)
+  #pygame.draw.circle(screen, (150, 150, 150), pos_2, rayon_2)
 
   pygame.display.flip()
-
-pygame.quit()
