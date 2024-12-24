2da332524af1e3c3073681285f9e2d7878c70397
Ewenn <fleter.ewenn@gmail.com>
2024-12-24 18:53:26 +0100

    ajout du cycle de la vie d'une étoile

--- space_simulation.py
+++ space_simulation.py
@@ -24,38 +24,80 @@ class Planet(pygame.sprite.Sprite):
 class Star(pygame.sprite.Sprite):
   def __init__(self):
     super().__init__()
-    self.radius = random.randint(5, 10)
+    self.radius = random.randint(5, 15)
+    self.velocity = 1
+    self.solarsys_id = 0
     self.x = random.randint(100, 1000)
     self.y = random.randint(50, 850)
-    self.color = (150, (255//self.radius)*3, 0)
-    self.life = (1000//self.radius)*5
+    self.Bcolor = int(self.radius*200/15)
+    self.color = (320-self.Bcolor, 125, self.Bcolor)
+    self.life = int(10**5-(self.radius*10**5)/15) # la durée de vie maximale d'une étoile est 10**5
 
   def draw(self, screen):
     pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
 
+  def death(self):
+    if self.radius > 13 :
+      self.color = (255, 255, 255)
+    elif self. radius >= 8 :
+      self.color = (255, 50, 50)
+      for i in range(50):
+        self.radius += 0.1
+        for planet in all_planets:
+          if star.y-star.radius == planet.y-planet.radius:
+            planet.kill()
+          if star.y-star.radius == planet.y-planet.radius:
+            planet.kill()
+      self.color = (255, 255, 255)
+    else:
+      self.radius = 5
+      self.color = (255, 255, 255)
+
 all_stars = pygame.sprite.Group()
-for i in range(10):
+for i in range(5):
   star = Star()
+  star.solarsys_id = i + 1
   all_stars.add(star)
 
 all_planets = pygame.sprite.Group()
+'''
 for i in range(10):
   planet = Planet()
   all_planets.add(planet)
+  '''
+chance_starborn = [1] + 100*[0]
 
 run = True
 while run:
+  print(chance_starborn)
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       run = False
 
   screen.fill((0, 0, 0))
 
+  if random.choice(chance_starborn) == 1:
+    chance_starborn.remove(0)
+    star = Star()
+    all_stars.add(star)
+
   for star in all_stars:
     star.draw(screen)
 
+    star.x += 1 * star.velocity
+    star.y += 1  * star.velocity
+
+    if star.life <= 0:
+      chance_starborn.append(0)
+      star.death()
+
     star.life -= 1
     print(star.life)
+
+    if star.x - star.radius < 0 or star.x + star.radius > 1100:
+      star.velocity *= -1
+    if star.y - star.radius < 0 or star.y + star.radius > 800:
+      star.velocity *= -1
 
   for planet in all_planets:
     planet.draw(screen)
